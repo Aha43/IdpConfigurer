dd2c255966083466a20590d1383201ceba658a58
Arne Halvorsen <arha@dips.no>
2023-01-29 22:14:12 +0100

    wip: added domain param class CreateIdpParam

---
+++ src/IdpAdmin.Domain/Param/CreateIdpParam.cs
@@ -0,0 +1,8 @@
+﻿namespace IdpAdmin.Domain.Param
+{
+    public record class CreateIdpParam
+    {
+        public required string Name { get; init; }
+        public required string Uri { get; init; }
+    }
+}
