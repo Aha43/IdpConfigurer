932ad33f72393f34db4b9ba24ef50386b1ac342f
Arne Halvorsen <arne.halvorsen@gmail.com>
2023-01-30 16:37:55 +0100

    wip: defined crud api for Idp

---
+++ src/IdentityModelManager.Specification/IIdpApi.cs
@@ -0,0 +1,12 @@
+﻿using IdentityModelManager.Domain;
+using IdentityModelManager.Domain.Param.Idp;
+
+namespace IdentityModelManager.Specification;
+
+public interface IIdpApi
+{
+    Task<Idp> CreateIdpAsync(CreateIdpParam param, CancellationToken cancellationToken);
+    Task<IEnumerable<Idp>> ReadIdpsAsync(GetIdpsParam param, CancellationToken cancellationToken);
+    Task<Idp> UpdateIdpAsync(Idp idp, CancellationToken cancellationToken);
+    Task<bool> DeleteIdpAsync(DeleteIdpParam param, CancellationToken cancellationToken);
+}
