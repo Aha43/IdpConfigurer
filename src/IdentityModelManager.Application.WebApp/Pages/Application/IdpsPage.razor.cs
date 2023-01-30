c9b46c9a9e505f38086be87302b933477247845b
Arne Halvorsen <arne.halvorsen@gmail.com>
2023-01-30 22:46:42 +0100

    wip: added page to show / create idps, no content yet

---
+++ src/IdentityModelManager.Application.WebApp/Pages/Application/IdpsPage.razor.cs
@@ -0,0 +1,13 @@
+using IdentityModelManager.Business.ViewController;
+using Microsoft.AspNetCore.Components;
+
+namespace IdentityModelManager.Application.WebApp.Pages.Application
+{
+    public partial class IdpsPage
+    {
+#nullable disable
+        [Inject] private IdpsViewController _viewController { get; set; }
+#nullable enable
+
+    }
+}
