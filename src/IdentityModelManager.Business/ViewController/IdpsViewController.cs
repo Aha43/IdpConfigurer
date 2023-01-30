e34ea5446fa4aeb7521108583a75badb854458ff
Arne Halvorsen <arne.halvorsen@gmail.com>
2023-01-30 17:54:11 +0100

    wip: started on view controller implementation for Idps

---
+++ src/IdentityModelManager.Business/ViewController/IdpsViewController.cs
@@ -0,0 +1,24 @@
+﻿using IdentityModelManager.Domain;
+using IdentityModelManager.Domain.Param.Idp;
+using IdentityModelManager.Specification;
+
+namespace IdentityModelManager.Business.ViewController
+{
+    public class IdpsViewController
+    {
+        private readonly IIdpApi _idpApi;
+
+        public IEnumerable<Idp> Idps { get; private set; } = Enumerable.Empty<Idp>();
+
+        public IdpsViewController(IIdpApi idpApi) => _idpApi = idpApi;
+
+        public async Task LoadAsync() => await LoadAsync(default).ConfigureAwait(false);
+        public async Task LoadAsync(CancellationToken cancellationToken)
+        {
+            var param = new GetIdpsParam();
+            Idps = await _idpApi.ReadIdpsAsync(param, cancellationToken).ConfigureAwait(false);
+        }
+
+    }
+
+}
