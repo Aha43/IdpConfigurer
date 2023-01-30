690e73e0ad9691ab5f21569abfd036d69f0eebca
Arne Halvorsen <arne.halvorsen@gmail.com>
2023-01-30 19:56:12 +0100

    wip: add IoC in business project

---
+++ src/IdentityModelManager.Business/Services.cs
@@ -0,0 +1,28 @@
+﻿using IdentityModelManager.Business.ViewController;
+using IdentityModelManager.Infrastructure.Memory;
+using IdentityModelManager.Specification;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityModelManager.Business
+{
+    public static class Services
+    {
+        public static IServiceCollection AddIdentityModelManagerInMemoryServices(this IServiceCollection services)
+        {
+            return services.AddSingleton<IIdpApi, InMemoryIdpRepository>().
+                AddIdentityModelManagerViewControllers();
+        }
+
+        private static IServiceCollection AddIdentityModelManagerViewControllers(this IServiceCollection services) 
+        {
+            return services.AddSingleton<IdpsViewController>();
+        }
+
+    }
+
+}
