8ba23689af3dd6d8d5ef105ad4a9a4828eee3398
Arne Halvorsen <arne.halvorsen@gmail.com>
2023-01-31 19:16:47 +0100

    wip: a first... extension methods to replace viewmodels! Made Idp extension method to give uri for its page

---
+++ src/IdentityModelManager.Business/DtoViewExtensions.cs
@@ -0,0 +1,13 @@
+﻿using IdentityModelManager.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityModelManager.Business;
+
+public static class DtoViewExtensions
+{
+    public static string PageUri(this Idp idp) => $"/Idp/{idp.Name}";
+}
