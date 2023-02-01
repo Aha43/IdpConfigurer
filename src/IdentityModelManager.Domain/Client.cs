84fac4d798c0c74a229fde0273747b50fd57310a
Arne Halvorsen <arne.halvorsen@gmail.com>
2023-02-01 13:09:55 +0100

    wip: added Client dto, for now only have id and name properties, add more as continue dev

---
+++ src/IdentityModelManager.Domain/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityModelManager.Domain
+{
+    public record class ClientId
+    {
+        public string? ClientId { get; set; }
+    }
+}
