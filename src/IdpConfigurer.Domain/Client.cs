992d87c4ac85450f0b0a32bba49fa8ae4ab5ce53
Arne Halvorsen <arha@dips.no>
2023-02-06 23:34:01 +0100

    wip: added ClientSecret domain class and added such secrets to Client domain class

--- src/IdpConfigurer.Domain/Client.cs
+++ src/IdpConfigurer.Domain/Client.cs
@@ -5,12 +5,14 @@
         public string? ClientId { get; set; }
         public string? ClientName { get; set; }
 
+        public ICollection<ClientSecret> ClientSecrets = new List<ClientSecret>();
+
         public bool AllowOfflineAccess { get; set; } = false;
         public bool AlwaysIncludeUserClaimsInIdToken { get; set; } = false;
 
-        public ICollection<string> AllowedGrantTypes { get; set; } = new HashSet<string>();
+        public ICollection<string> AllowedGrantTypes = new HashSet<string>();
 
-        public ICollection<string> RedirectUris { get; set; } = new HashSet<string>();
-        public ICollection<string> AllowedScopes { get; set; } = new HashSet<string>();
+        public ICollection<string> RedirectUris = new HashSet<string>();
+        public ICollection<string> AllowedScopes = new HashSet<string>();
     }
 }
---
+++ src/IdpConfigurer.Domain/ClientSecret.cs
@@ -0,0 +1,8 @@
+﻿namespace IdpConfigurer.Domain;
+
+public record class ClientSecret
+{
+    public string Type { get; set; } = "SharedSecret";
+    public string Description { get; set; } = string.Empty;
+    public required string Value { get; set; }
+}
