d96ebad662765efa0c570a5c85cc9e3a8a16790f
Arne Halvorsen <arha@dips.no>
2023-02-06 20:55:20 +0100

    wip: added AlwaysIncludeUserClaimsInIdToken property to Client domain class

--- src/IdpConfigurer.Domain/Client.cs
+++ src/IdpConfigurer.Domain/Client.cs
@@ -6,6 +6,7 @@
         public string? ClientName { get; set; }
 
         public bool AllowOfflineAccess { get; set; } = false;
+        public bool AlwaysIncludeUserClaimsInIdToken { get; set; } = false;
 
         public ICollection<string> RedirectUris { get; set; } = new HashSet<string>();
         public ICollection<string> AllowedScopes { get; set; } = new HashSet<string>();
