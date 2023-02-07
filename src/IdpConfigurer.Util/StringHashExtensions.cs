61b2e70f00edc18ee6041701d1afbfa2552a9ad2
Arne Halvorsen <arha@dips.no>
2023-02-07 08:32:06 +0100

    wip: StringHashExtensions in util project

---
+++ src/IdpConfigurer.Util/StringHashExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace IdpConfigurer.Util;
+
+public static class StringHashExtensions
+{
+    public static string Sha256(this string input)
+    {
+        var bytes = Encoding.UTF8.GetBytes(input);
+        var hash = SHA256.HashData(bytes);
+        return Convert.ToBase64String(hash);
+    }
+
+    public static string Sha512(this string input)
+    {
+        var bytes = Encoding.UTF8.GetBytes(input);
+        var hash = SHA256.HashData(bytes);
+        return Convert.ToBase64String(hash);
+    }
+
+}
