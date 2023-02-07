e6b83a128e6d3248237a895eda49c86b31ebddf6
Arne Halvorsen <arha@dips.no>
2023-02-07 10:17:29 +0100

    wip: gui for adding post logout redirect uris

--- src/IdpConfigurer.Business/ViewController/ClientViewController.cs
+++ src/IdpConfigurer.Business/ViewController/ClientViewController.cs
@@ -88,7 +88,6 @@ public class ClientViewController
     #endregion
 
     #region redirectUris
-
     public string? RedirectUriErrorMessage = null;
 
     public string? NewRedirectUri { get; set; }
@@ -122,6 +121,44 @@ public class ClientViewController
         if (Client == null) return;
         
         Client.RedirectUris.Remove(uri);
+        await UpdateClient(cancellationToken).ConfigureAwait(false);
+    }
+    #endregion
+
+    #region PostLogoutRedirectUris
+    public string? PostLogoutRedirectUriErrorMessage = null;
+
+    public string? NewPostLogoutRedirectUri { get; set; }
+
+    public async Task AddPostLogoutRedirectUriAsync() => await AddPostLogoutRedirectUriAsync(default).ConfigureAwait(false);
+    public async Task AddPostLogoutRedirectUriAsync(CancellationToken cancellationToken)
+    {
+        PostLogoutRedirectUriErrorMessage = null;
+
+        if (Client == null) return;
+        if (string.IsNullOrEmpty(NewPostLogoutRedirectUri)) return;
+
+        NewPostLogoutRedirectUri = NewPostLogoutRedirectUri!.Trim();
+        if (!Client.PostLogoutRedirectUris.Contains(NewPostLogoutRedirectUri))
+        {
+            if (!NewPostLogoutRedirectUri.ValidAbsoluteUri())
+            {
+                PostLogoutRedirectUriErrorMessage = $"Redirect URI '{NewPostLogoutRedirectUri}' not valid absolute URI";
+                return;
+            }
+
+            Client.PostLogoutRedirectUris.Add(NewPostLogoutRedirectUri);
+            await UpdateClient(cancellationToken).ConfigureAwait(false);
+            NewPostLogoutRedirectUri = null;
+        }
+    }
+
+    public async Task RemovePostLogoutRedirectUriAsync(string uri) => await RemovePostLogoutRedirectUriAsync(uri, default).ConfigureAwait(false);
+    public async Task RemovePostLogoutRedirectUriAsync(string uri, CancellationToken cancellationToken)
+    {
+        if (Client == null) return;
+
+        Client.PostLogoutRedirectUris.Remove(uri);
         await UpdateClient(cancellationToken).ConfigureAwait(false);
     }
     #endregion
