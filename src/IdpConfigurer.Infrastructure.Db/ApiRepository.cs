6b4567f35c98ca7961dad6255bb9709703d70332
Arne Halvorsen <arha@dips.no>
2023-02-08 10:34:46 +0100

    wip: added dapper to db project, declared first repository go on to implement its IApiScopeApi methods callin stored procedures

---
+++ src/IdpConfigurer.Infrastructure.Db/ApiRepository.cs
@@ -0,0 +1,43 @@
+﻿using IdpConfigurer.Domain;
+using IdpConfigurer.Domain.Param.ApiScope;
+using IdpConfigurer.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdpConfigurer.Infrastructure.Db
+{
+    public class ApiRepository : IApiScopeApi
+    {
+        private readonly ConnectionProvider _connectionProvider;
+
+        public ApiRepository(ConnectionProvider connectionProvider) => _connectionProvider = connectionProvider;
+
+        public Task<ApiScope> CreateApiScopeAsync(CreateApiScopeParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteApiScopeAsync(DeleteApiScopeParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiScope> ReadApiScopeAsync(ReadApiScopeParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApiScope>> ReadApiScopesAsync(ReadApiScopesParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiScope> UpdateApiScopeAsync(UpdateApiScopeParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
