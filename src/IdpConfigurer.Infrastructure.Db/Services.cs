afc1fa9af4b4c1ea905d22b4db574a3e9f01c78f
Arne Halvorsen <arha@dips.no>
2023-02-08 09:18:12 +0100

    wip: added db project and web api project

---
+++ src/IdpConfigurer.Infrastructure.Db/Services.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace IdpConfigurer.Infrastructure.Db;
+
+public static class Services
+{
+    public static IServiceCollection AddDbInfrastructure(this IServiceCollection services, IConfiguration configration)
+    {
+        var connectionString = configration.GetConnectionString("default");
+        if (connectionString == null) return services;
+        return services.AddSingleton(new ConnectionProvider(connectionString));
+    }
+}
+
+public class ConnectionProvider
+{
+    private readonly string _connectionString;
+
+    public ConnectionProvider(string connectionString) => _connectionString = connectionString;
+
+    public DbConnection Connection => new SqlConnection(_connectionString);
+}
