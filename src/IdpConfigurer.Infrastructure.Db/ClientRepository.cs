e0acace92ded8ae4855459dd8d56bb7862d75d0c
Arne Halvorsen <arha@dips.no>
2023-02-08 18:44:30 +0100

    wip: declared client repository in db project => go on to implement its IApiScopeApi methods calling stored procedures

---
+++ src/IdpConfigurer.Infrastructure.Db/ClientRepository.cs
@@ -0,0 +1,34 @@
+﻿using IdpConfigurer.Domain;
+using IdpConfigurer.Domain.Param.Client;
+using IdpConfigurer.Specification;
+
+namespace IdpConfigurer.Infrastructure.Db
+{
+    public class ClientRepository : IClientApi
+    {
+        public Task<Client> CreateClientAsync(CreateClientParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteClientAsync(DeleteClientParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> ReadClientAsync(ReadClientParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Client>> ReadClientsAsync(ReadClientsParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Client> UpdateClientAsync(UpdateClientParam param, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
