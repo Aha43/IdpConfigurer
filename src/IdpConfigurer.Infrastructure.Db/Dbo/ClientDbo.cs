db70c7296c3612acfd2a284c651ec9eaab01a03f
Arne Halvorsen <arha@dips.no>
2023-02-08 19:25:41 +0100

    wip: implemented needed dbo for the db project client repository

---
+++ src/IdpConfigurer.Infrastructure.Db/Dbo/ClientDbo.cs
@@ -0,0 +1,53 @@
+﻿using IdpConfigurer.Domain;
+using Newtonsoft.Json;
+
+namespace IdpConfigurer.Infrastructure.Db.Dbo;
+
+public class ClientDbo
+{
+    public string? ClientId { get; set; }
+    public string? ClientName { get; set; }
+    public string? IdpName { get; set;}
+    public string? Json { get; set; }
+}
+
+
+public static class Extensions
+{
+    public static ClientDbo ToDbo(this Client client, string idpName)
+    {
+        return new ClientDbo
+        {
+            ClientId = client.ClientId,
+            ClientName = client.ClientName,
+            IdpName = idpName,
+            Json = JsonConvert.SerializeObject(client)
+        };
+    }
+
+    public static Client ToClient(this ClientDbo dbo)
+    {
+        if (dbo.Json == null)
+        {
+            throw new ArgumentNullException(nameof(dbo.Json));
+        }
+
+        var retVal = JsonConvert.DeserializeObject<Client>(dbo.Json);
+        if (retVal == null) 
+        {
+            throw new ArgumentException("Parsing of json produced null");
+        }
+
+        if (retVal.ClientId != null && !retVal.ClientId.Equals(dbo.ClientId))
+        {
+            throw new ArgumentException($"Expected client id : '{dbo.ClientId}' (from dbo) but found '{retVal.ClientId}' in json");
+        }
+        if (retVal.ClientName != null && !retVal.ClientName.Equals(dbo.ClientName))
+        {
+            throw new ArgumentException($"Expected clientName : '{dbo.ClientName}' (from dbo) but found '{retVal.ClientName}' in json");
+        }
+
+        return retVal;
+    }
+
+}
