85a2895b14cf77fc4e48224c32e78cd561bacab3
Arne Halvorsen <arha@dips.no>
2023-02-08 18:32:20 +0100

    wip: implemented all of methods in ApiRepository

--- src/IdpConfigurer.Infrastructure.Db/ApiRepository.cs
+++ src/IdpConfigurer.Infrastructure.Db/ApiRepository.cs
@@ -20,27 +20,55 @@ public class ApiRepository : IApiScopeApi
         dp.Add("@Name", param.Name);
         dp.Add("@displayName", param.DisplayName);
 
-        var retVal = await con.QueryAsync<ApiScope>("idpc.CreateApi", dp, commandType: System.Data.CommandType.StoredProcedure).ConfigureAwait(false);
-        return retVal.First();
+        var result = await con.QueryAsync<ApiScope>("idpc.CreateApi", dp, commandType: System.Data.CommandType.StoredProcedure).ConfigureAwait(false);
+        return result.First();
     }
 
-    public Task DeleteApiScopeAsync(DeleteApiScopeParam param, CancellationToken cancellationToken)
+    public async Task DeleteApiScopeAsync(DeleteApiScopeParam param, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        using var con = _connectionProvider.Connection;
+
+        var dp = new DynamicParameters();
+        dp.Add("@ipdName", param.IdpName);
+        dp.Add("@Name", param.Name);
+
+        await con.ExecuteAsync("idpc.CreateApi", commandType: System.Data.CommandType.StoredProcedure).ConfigureAwait(false);
     }
 
-    public Task<ApiScope> ReadApiScopeAsync(ReadApiScopeParam param, CancellationToken cancellationToken)
+    public async Task<ApiScope> ReadApiScopeAsync(ReadApiScopeParam param, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        using var con = _connectionProvider.Connection;
+
+        var dp = new DynamicParameters();
+        dp.Add("@ipdName", param.IdpName);
+        dp.Add("@Name", param.Name);
+
+        var result = await con.QueryAsync<ApiScope>("idpc.ReadApi", dp, commandType: System.Data.CommandType.StoredProcedure).ConfigureAwait(false);
+        return result.First();
     }
 
-    public Task<IEnumerable<ApiScope>> ReadApiScopesAsync(ReadApiScopesParam param, CancellationToken cancellationToken)
+    public async Task<IEnumerable<ApiScope>> ReadApiScopesAsync(ReadApiScopesParam param, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        using var con = _connectionProvider.Connection;
+
+        var dp = new DynamicParameters();
+        dp.Add("@ipdName", param.IdpName);
+
+        var retVal = await con.QueryAsync<ApiScope>("idpc.ReadApi", dp, commandType: System.Data.CommandType.StoredProcedure).ConfigureAwait(false);
+        return retVal;
     }
 
-    public Task<ApiScope> UpdateApiScopeAsync(UpdateApiScopeParam param, CancellationToken cancellationToken)
+    public async Task<ApiScope> UpdateApiScopeAsync(UpdateApiScopeParam param, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        using var con = _connectionProvider.Connection;
+
+        var dp = new DynamicParameters();
+        dp.Add("@ipdName", param.IdpName);
+        dp.Add("@Name", param.ApiScope.Name);
+        dp.Add("@displayName", param.ApiScope.DisplayName);
+
+        var result = await con.QueryAsync<ApiScope>("idpc.CreateApi", dp, commandType: System.Data.CommandType.StoredProcedure).ConfigureAwait(false);
+        return result.First();
     }
+
 }
