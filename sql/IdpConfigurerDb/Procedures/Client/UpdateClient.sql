94f147c1c564a950232a560e02e284d82a20a414
Arne Halvorsen <arha@dips.no>
2023-02-09 12:57:04 +0100

    fix: db persistence of client, now db persistence starting to look good!

--- sql/IdpConfigurerDb/Procedures/Client/UpdateClient.sql
+++ sql/IdpConfigurerDb/Procedures/Client/UpdateClient.sql
@@ -2,8 +2,9 @@
 	@idpName nvarchar(40),
 	@clientId nvarchar(40),
 	@clientName nvarchar(40),
-	@json nvarchar(40)
+	@json nvarchar(max)
 AS
-	UPDATE [idpc].[Client] SET [ClientName] = @clientName, [Json] = @json WHERE ClientId = @clientId AND IdpName = @idpName 
+	UPDATE [idpc].[Client] SET [ClientName] = @clientName, [Json] = @json WHERE [ClientId] = @clientId AND [IdpName] = @idpName 
+	SELECT * FROM [idpc].[Client] t WHERE t.[ClientId] = @clientId AND t.[IdpName] = @idpName
 RETURN 0
 GO
