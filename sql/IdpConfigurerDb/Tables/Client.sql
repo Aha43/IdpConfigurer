4856d87d76e9cdae1684d9c6c7af419e803996c5
Arne Halvorsen <arha@dips.no>
2023-02-09 11:39:37 +0100

    fix: json column of Client table only one character wide...

--- sql/IdpConfigurerDb/Tables/Client.sql
+++ sql/IdpConfigurerDb/Tables/Client.sql
@@ -3,7 +3,7 @@
 	[ClientId] nvarchar(40) NOT NULL,
 	[ClientName] nvarchar(40) NOT NULL,
 	[IdpName] nvarchar(40) NOT NULL,
-	[Json] nvarchar NOT NULL,
+	[Json] nvarchar(max) NOT NULL,
 
 	CONSTRAINT FK_Client_Idp FOREIGN KEY (IdpName)
 	REFERENCES [idpc].Idp(Name)
