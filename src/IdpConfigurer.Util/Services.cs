4d67b0057e0856c3de6edf1cf76032f2d546f63e
Arne Halvorsen <arha@dips.no>
2023-02-10 20:23:24 +0100

    wip: work on shared secret

--- src/IdpConfigurer.Util/Services.cs
+++ src/IdpConfigurer.Util/Services.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using IdpConfigurer.Specification.Tool;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IdpConfigurer.Util;
@@ -7,6 +8,6 @@ public static class Services
 {
     public static IServiceCollection AddUtilities(this IServiceCollection services, IConfiguration configuration)
     {
-
+        return services.AddSingleton<ISharedGenerator, Sha256SharedSecretGenerator>();
     }
 }
