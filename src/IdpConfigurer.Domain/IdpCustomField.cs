b2cfa69b2157e0f0c0c06c7f6c2502b4e4d7a615
Arne Halvorsen <arha@dips.no>
2023-02-12 18:44:50 +0100

    wip: started to doodle on Idp custom data

---
+++ src/IdpConfigurer.Domain/IdpCustomField.cs
@@ -0,0 +1,10 @@
+﻿namespace IdpConfigurer.Domain
+{
+    public record class IdpCustomField
+    {
+        public required string Name { get; set; }
+        public required string Type { get; set; }
+        public string? Value { get; set; }
+        public string Default { get; set; }
+    }
+}
