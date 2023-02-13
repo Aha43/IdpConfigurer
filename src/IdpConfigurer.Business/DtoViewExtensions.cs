d16ac9647bc73de3dcffc1344c83855d5f3ef0dc
Arne Halvorsen <arha@dips.no>
2023-02-13 20:44:30 +0100

    wip: added list of client usin the api on api scope page

--- src/IdpConfigurer.Business/DtoViewExtensions.cs
+++ src/IdpConfigurer.Business/DtoViewExtensions.cs
@@ -7,6 +7,7 @@ public static class DtoViewExtensions
     public static string PageUri(this Idp idp) => $"/Idp/{idp.Name}";
 
     public static string PageUri(this Client client, Idp idp) => $"/Idp/{idp.Name}/Client/{client.ClientId}";
+    public static string PageUri(this Client client, string idpName) => $"/Idp/{idpName}/Client/{client.ClientId}";
 
     public static string Title(this ApiScope apiScope) => string.IsNullOrWhiteSpace(apiScope.DisplayName) ? apiScope.Name : apiScope.DisplayName;
     public static string PageUri(this ApiScope apiScope, Idp idp) => $"/Idp/{idp.Name}/ApiScope/{apiScope.Name}";
--- src/IdpConfigurer.Business/ViewController/ApiScopeViewController.cs
+++ src/IdpConfigurer.Business/ViewController/ApiScopeViewController.cs
@@ -1,5 +1,6 @@
 ﻿using IdpConfigurer.Domain;
 using IdpConfigurer.Domain.Param.ApiScope;
+using IdpConfigurer.Domain.Param.Client;
 using IdpConfigurer.Specification.Api;
 
 namespace IdpConfigurer.Business.ViewController
@@ -7,12 +8,21 @@ namespace IdpConfigurer.Business.ViewController
     public class ApiScopeViewController
     {
         private readonly IApiScopeApi _apiScopeApi;
+        private readonly IClientApi _clientApi;
 
         public string? IdpName { get; private set; }
 
         public ApiScope? ApiScope { get; private set; }
 
-        public ApiScopeViewController(IApiScopeApi apiScopeApi) => _apiScopeApi = apiScopeApi;
+        public IEnumerable<Client> Clients { get; private set; } = Enumerable.Empty<Client>();
+
+        public ApiScopeViewController(
+            IApiScopeApi apiScopeApi,
+            IClientApi clientApi)
+        {
+            _apiScopeApi = apiScopeApi;
+            _clientApi = clientApi;
+        }
 
         public async Task LoadAsync(string idpName, string apiName) => await LoadAsync(idpName, apiName, default).ConfigureAwait(false);
         public async Task LoadAsync(string idpName, string apiName, CancellationToken cancellationToken)
@@ -21,6 +31,12 @@ namespace IdpConfigurer.Business.ViewController
 
             var readApiScopeParam = new ReadApiScopeParam { IdpName = idpName, Name = apiName };
             ApiScope = await _apiScopeApi.ReadApiScopeAsync(readApiScopeParam, cancellationToken).ConfigureAwait(false);
+
+            var readClientsParam = new ReadClientsParam { IdpName = idpName };
+            var clients = await _clientApi.ReadClientsAsync(readClientsParam, cancellationToken).ConfigureAwait(false);
+            Clients = clients.Where(e => e.AllowedScopes.Contains(apiName)).ToList();
         }
+
     }
+
 }
