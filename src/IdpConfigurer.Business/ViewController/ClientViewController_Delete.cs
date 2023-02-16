cf35fd2c186699e4f7745ba6bfe8f3e70aab70a0
Arne Halvorsen <arha@dips.no>
2023-02-16 04:36:53 +0100

    feat: #7 delete client

---
+++ src/IdpConfigurer.Business/ViewController/ClientViewController_Delete.cs
@@ -0,0 +1,22 @@
+﻿using IdpConfigurer.Domain.Param.Client;
+
+namespace IdpConfigurer.Business.ViewController
+{
+    public partial class ClientViewController
+    {
+        public bool Deleted { get; private set; } = false;
+
+        public async Task DeleteAsync() => await DeleteAsync(default);
+        public async Task DeleteAsync(CancellationToken cancellationToken)
+        {
+            if (Client == null || IdpName == null) return;
+
+            var deleteParam = new DeleteClientParam { IdpName = IdpName, ClientId = Client.ClientId };
+            await _clientApi.DeleteClientAsync(deleteParam, cancellationToken).ConfigureAwait(false);
+
+            Deleted = true;
+        }
+
+    }
+
+}
