916d50b406240f697adb30ebcfa161134ed0f614
Arne Halvorsen <arha@dips.no>
2023-02-17 09:00:02 +0100

    wip: added properties to IdpData as dictated by #33

--- src/IdpConfigurer.Domain/IdpData.cs
+++ src/IdpConfigurer.Domain/IdpData.cs
@@ -2,5 +2,11 @@
 
 public record class IdpData
 {
+    public string? Description { get; set; }
+    public string? ServiceModel { get; set; }
+    public string? ServiceVersion { get; set; }
+    public int MaxNumberOfProducts { get; set; }
+    public IEnumerable<string> Products { get; set; } = Enumerable.Empty<string>();
+    public int MaxNumberOfClients { get; set; }
     public IdpCustomData CustomData { get; set; } = new();
 }
