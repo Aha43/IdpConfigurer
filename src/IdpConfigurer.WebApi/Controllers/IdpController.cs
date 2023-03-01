ea3bc387790da24a5273d91f5a4619a7dd552387
Arne Halvorsen <arha@dips.no>
2023-03-01 13:55:26 +0100

    wip: save commit, decided to check out minimal api in separate branch

---
+++ src/IdpConfigurer.WebApi/Controllers/IdpController.cs
@@ -0,0 +1,12 @@
+﻿using IdpConfigurer.Specification.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdpConfigurer.WebApi.Controllers
+{
+    public class IdpController : ControllerBase
+    {
+        private readonly IIdpApi _idpApi;
+
+        public IdpController(IIdpApi idpApi) => _idpApi = idpApi;
+    }
+}
